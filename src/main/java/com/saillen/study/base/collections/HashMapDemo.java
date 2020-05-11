bb2b08a42c358bc8a75ba4c3f0fb5d2d636fbfcb
saillen <saillen_aws@163.com>
2020-05-11 23:17:26 +0800

    添加一些 demo

---
+++ src/main/java/com/saillen/study/base/collections/HashMapDemo.java
@@ -0,0 +1,19 @@
+package com.saillen.study.base.collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author : saillen
+ * @date: 2020/5/11
+ **/
+public class HashMapDemo {
+
+    public static void main(String[] args) {
+        Map map = new HashMap<>();
+        map.put(1, 1);
+        map.put(1, 2);
+        map.put(17, 2);
+    }
+
+}
